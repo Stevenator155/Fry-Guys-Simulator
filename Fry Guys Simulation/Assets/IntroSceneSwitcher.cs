db9af4ffde941ca184b85e54d82b7aa9a9c928f4
BurntToast <steven@jasonsmith.net>
2024-04-16 19:38:09 -0500

    Added intro video, Switched fps controller, Lit up the building and slightly rescaled it

---
+++ Fry Guys Simulation/Assets/IntroSceneSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+public class IntroSceneSwitcher : MonoBehaviour
+{
+    IEnumerator SwtichOnEnd()
+    {
+        yield return new WaitForSeconds(1f);
+        while (this.GetComponent<VideoPlayer>().isPlaying)
+        {
+            yield return null;
+        }
+        SceneManager.LoadScene("Menu");
+    }
+
+    private void Awake()
+    {
+        this.GetComponent<VideoPlayer>().Play();
+        StartCoroutine(SwtichOnEnd());
+    }
+}
--- Fry Guys Simulation/Assets/MenuFramework.cs
+++ Fry Guys Simulation/Assets/MenuFramework.cs
@@ -1,10 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuFramework : MonoBehaviour
 {
-   public void ExitButton()
+     IEnumerator StartGame()
+    {
+        transform.Find("AS").gameObject.SetActive(false);
+        transform.Find("fd").GetComponent<Animation>().Play("Fade");
+        while (transform.Find("fd").GetComponent<Animation>().isPlaying)
+        {
+            yield return null;
+        }
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void StartStartCoro()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    public void ExitButton()
     {
         Application.Quit();
     }
