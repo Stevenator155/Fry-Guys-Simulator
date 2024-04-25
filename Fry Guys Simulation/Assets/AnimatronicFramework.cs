9bcb06ad18c0e470e4509d560f1723509228e8b8
BurntToast <steven@jasonsmith.net>
2024-04-25 18:44:13 -0500

    Animatronic AI Progress

---
+++ Fry Guys Simulation/Assets/AnimatronicFramework.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AnimatronicFramework : MonoBehaviour
+{
+    NavMeshAgent Agent;
+    GameObject Character;
+    GameObject[] Points;
+    private const float rotSpeed = 20f;
+
+    void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        Character = GameObject.Find("Character");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Agent.SetDestination(Character.transform.position);
+
+    }
+}
